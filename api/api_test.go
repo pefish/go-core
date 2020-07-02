b0b49e73248202580523b34fb20a73a5832844b5
pefish <pefish@qq.com>
2020-07-02 17:37:14 +0800

    Add unit test for Api

--- api/api_test.go
+++ api/api_test.go
@@ -11,34 +11,52 @@ import (
 )
 
 func TestWrapJson(t *testing.T) {
-	handler := WrapJson(map[string]*Api{
-		string(api_session.ApiMethod_All): &Api{
-			Description:            "test",
-			Path:                   "/",
-			IgnoreRootPath:         true,
-			IgnoreGlobalStrategies: true,
-			Method:                 api_session.ApiMethod_All,
-			Controller: func(apiSession *api_session.ApiSessionClass) interface{} {
-				apiSession.WriteText(`hahahahahahahaha`)
-				return nil
-			},
-			ParamType: global_api_strategy.ALL_TYPE,
-		},
-	})
-
 	var result string
 	ctrl := gomock.NewController(t)
 	httpResponseWriter := mock_http.NewMockResponseWriter(ctrl)
 	httpResponseWriter.EXPECT().Write(gomock.AssignableToTypeOf([]byte{})).DoAndReturn(func(args []byte) (int, error) {
 		result = string(args)
 		return len(args), nil
-	})
-	httpResponseWriter.EXPECT().WriteHeader(gomock.AssignableToTypeOf(1))
+	}).AnyTimes()
+	httpResponseWriter.EXPECT().WriteHeader(gomock.AssignableToTypeOf(1)).AnyTimes()
 	httpResponseWriter.EXPECT().Header().Return(http.Header{}).AnyTimes()
+
+
+	handler := WrapJson(map[string]*Api{
+		string(api_session.ApiMethod_Get): &Api{
+			Description:            "test",
+			Path:                   "/",
+			IgnoreRootPath:         true,
+			IgnoreGlobalStrategies: true,
+			Method:                 api_session.ApiMethod_Get,
+			Controller: func(apiSession *api_session.ApiSessionClass) interface{} {
+				apiSession.WriteText(`this is a get api`)
+				return nil
+			},
+			ParamType: global_api_strategy.ALL_TYPE,
+		},
+		string(api_session.ApiMethod_Post): &Api{
+			Description:            "test1",
+			Path:                   "/",
+			IgnoreRootPath:         true,
+			IgnoreGlobalStrategies: true,
+			Method:                 api_session.ApiMethod_Post,
+			Controller: func(apiSession *api_session.ApiSessionClass) interface{} {
+				apiSession.WriteText(`this is a post api`)
+				return nil
+			},
+			ParamType: global_api_strategy.ALL_TYPE,
+		},
+	})
+	handler(httpResponseWriter, &http.Request{
+		Method: "GET",
+		Header: http.Header{},
+	})
+	test.Equal(t, "this is a get api", result)
+
 	handler(httpResponseWriter, &http.Request{
 		Method: "POST",
 		Header: http.Header{},
 	})
-
-	test.Equal(t, "hahahahahahahaha", result)
+	test.Equal(t, "this is a post api", result)
 }
