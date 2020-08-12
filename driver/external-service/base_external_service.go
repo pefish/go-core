3f057b3eb535d3989f8675ca04170ddf7a628c96
pefish <pefish@qq.com>
2020-08-12 18:50:02 +0800

    Fix

--- driver/external-service/base_external_service.go
+++ driver/external-service/base_external_service.go
@@ -3,20 +3,27 @@ package external_service
 import (
 	"encoding/json"
 	"errors"
+	go_application "github.com/pefish/go-application"
 	"github.com/pefish/go-core/api"
+	"github.com/pefish/go-core/driver/logger"
 	"github.com/pefish/go-error"
 	"github.com/pefish/go-http"
+	"time"
 )
 
 
 // 让外部服务可以通过这个基类调用内部功能
 type BaseExternalServiceClass struct {
-
+	timeout time.Duration
 }
 
 
 func (bes *BaseExternalServiceClass) Init(driver *ExternalServiceDriverClass) {
+	bes.timeout = 20 * time.Second
+}
 
+func (bes *BaseExternalServiceClass) SetTimeout(timeout time.Duration) {
+	bes.timeout = timeout
 }
 
 func (bes *BaseExternalServiceClass) PostJsonForStruct(url string, params map[string]interface{}, struct_ interface{}) *go_error.ErrorInfo {
@@ -37,7 +44,7 @@ func (bes *BaseExternalServiceClass) PostJsonForStruct(url string, params map[st
 
 func (bes *BaseExternalServiceClass) PostJson(url string, params map[string]interface{}) (interface{}, *go_error.ErrorInfo) {
 	result := api.ApiResult{}
-	_, err := go_http.NewHttpRequester().PostForStruct(go_http.RequestParam{
+	_, err := go_http.NewHttpRequester(go_http.WithIsDebug(go_application.Application.Debug), go_http.WithLogger(logger.LoggerDriver.Logger), go_http.WithTimeout(bes.timeout)).PostForStruct(go_http.RequestParam{
 		Url:    url,
 		Params: params,
 	}, &result)
@@ -68,7 +75,7 @@ func (bes *BaseExternalServiceClass) GetJsonForStruct(url string, params map[str
 
 func (bes *BaseExternalServiceClass) GetJson(url string, params map[string]interface{}) (interface{}, *go_error.ErrorInfo) {
 	result := api.ApiResult{}
-	_, err := go_http.NewHttpRequester().GetForStruct(go_http.RequestParam{
+	_, err := go_http.NewHttpRequester(go_http.WithIsDebug(go_application.Application.Debug), go_http.WithLogger(logger.LoggerDriver.Logger), go_http.WithTimeout(bes.timeout)).GetForStruct(go_http.RequestParam{
 		Url:    url,
 		Params: params,
 	}, &result)
